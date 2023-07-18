6b1bf1d8023ad0cec27ce0bbd1310ba4c738b8e3
FVazquezRisso <facundovazquez321@gmail.com>
2023-07-18 20:57:17 -0300

    Sin deploy

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const server = require("./src/server");
 const { conn } = require('./src/db.js');
-const llenarBD = require('./llenarDB')
-require("dotenv").config();
+const llenarBD = require("./llenarDB");
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 
 conn.sync({ force: true }).then(() => {
 server.listen(PORT, async () => {
